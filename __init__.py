e21f5895d1682152561958b319354d77939bd90d
Shadow <94984080+TrueShadow01@users.noreply.github.com>
2026-08-28 19:39:41 +0200

    Version Bump

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 bl_info = {
     "name": "RE Mesh Editor (Community Maintained)",
     "author": "NSA Cloud, TrueShadow",
-    "version": (0, 72, 0),
+    "version": (0, 72, 5),
     "blender": (4, 3, 2),
     "location": "File > Import-Export",
     "description": "Import and export RE Engine Mesh files natively into Blender. No Noesis required.",
